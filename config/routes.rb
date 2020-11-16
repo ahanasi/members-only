a0616e8933403719802a96bf76be3190b88a104e
ahanasi <ahanasi@seas.upenn.edu>
2020-11-15 17:02:05 -0800

    Add layouts for Devise User Registrations and Log In

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+            registrations: "users/registrations",
+          }
+  resources :posts, only: [:new, :create, :index]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "posts#index"
+end
